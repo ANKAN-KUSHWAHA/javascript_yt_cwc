2942ffdb6706e87bf898d5ccf94dbee55e85ce61
Ankan Kushwaha <ankankushwaha30@gmail.com>
2025-04-30 14:33:40 +0000

    commit

---
+++ basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 144943
+let accountEmail="ankan@gmail.com"
+var accountPassword="29349"
+
+accountCity="puranpur"
+
+let accountState;
+
+// accountId=202 // not allowed
+accountEmail="pk@gmail.com"
+accountPassword="11111"
+accountCity="pnp"
+
+/* prefer not to use var , because of issue of block scope and function scope */
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
