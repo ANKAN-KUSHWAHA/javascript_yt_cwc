70de043bf79edfaf78a90614ea99f8d97fe9992a
Ankan Kushwaha <ankankushwaha30@gmail.com>
2025-05-01 06:05:10 +0000

    commit

---
+++ basics/04_compairision.js
@@ -0,0 +1,26 @@
+/*console.log(2>1); // true
+console.log(1 ==2); //false
+
+console.log("1"> 2); // js will automatically convert string to number for compairison
+
+console.log("02" > 1);
+
+console.log(null>0);// false , same for undefined
+console.log(null<0); // false , same for underfined
+console.log(null==0); // false , same for undefined
+
+console.log(null >=0); // true null would be converted into 0
+*/
+/// strict check { check value with datatype as well }
+
+console.log("2"===2); // because it check datatype
+
+
+
+
+
+
+
+
+
+
