19b81ed4c3e5210c0e3962da35003b27680fa981
Ankan Kushwaha <ankankushwaha30@gmail.com>
2025-05-01 03:49:56 +0000

    commit

---
+++ basics/02_datatype.js
@@ -0,0 +1,26 @@
+console.log(3
+    +3
+    ); // code readability should be high
+
+let name="ankan"
+let age =18;
+let isLoggedIn=false;
+let state = null
+console.log(typeof state);//object
+
+/**
+ * datatye in javascript
+ * number => 2 to power 53
+ * bigint =>
+ * string => "" or ''
+ * boolean => true/false
+ * null => it is datatype and also a stand alone value
+ * undefined => abhi value defined nahi hai
+ * symbol => unique
+ * 
+ * 
+ * /// object
+ */
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
