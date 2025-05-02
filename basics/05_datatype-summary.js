f04f0f09d6633de7bf576a3f9ab56e1ac7df9ac9
Ankan Kushwaha <ankankushwaha30@gmail.com>
2025-05-02 18:10:57 +0000

    commit

---
+++ basics/05_datatype-summary.js
@@ -0,0 +1,46 @@
+/**
+ * ####### Primitive ####
+ * 
+ * # 7types : String,Number , Boolean,null , undefined ,symbol,bigInt
+ */
+const score= 100;
+const scoreValue =100.3;
+const isLoggedIn =false;
+const outsideTemp=null;
+let userEmail;
+
+const id=Symbol('123'); /// symbol used for defining unique things
+const anotherId=Symbol('123');
+
+console.log(id===anotherId);
+
+const bigNumber=848484848938384n;
+
+/**
+ * Refrence (Non primitive)
+ * // Array, Objects, Functions
+ * typeof of non primitive is object
+ */
+
+const heroes=["ironman","Saktiman"];
+let myObj = {
+    name: "Ankan",
+    age:22,
+}
+
+const myFunction=function(){
+    console.log("Hello World");
+    
+}
+console.log(typeof bigNumber);
+console.log( typeof outsideTemp);
+console.log(typeof myObj);
+console.log(typeof myFunction); // object function
+console.log(typeof heroes);
+
+
+
+
+
+
+
