21e4b90dd892fa2d54d1b8bf453a094024d640b2
Ankan Kushwaha <ankankushwaha30@gmail.com>
2025-05-03 14:39:14 +0000

    commit

--- basics/05_datatype-summary.js
+++ basics/05_datatype-summary.js
@@ -39,8 +39,21 @@ console.log(typeof myFunction); // object function
 console.log(typeof heroes);
 
 
+/**stack(for primitive) , heap (non-primitive) */
 
+let myYoutubename="ankanKushwaha";
+let anotherName =myYoutubename;
+anotherName="prateekKushwaha"
 
+console.log(anotherName);
 
+let user={
+    email: "ankanKushwaha@gmail.com",
+    upi:"49494930@ybl"
+}
+let usertwo=user;
+usertwo.email="prateekkushwaha@gmail.com";
 
+console.log(user.email);
+console.log(usertwo.email);
 
