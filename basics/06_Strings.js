fadfdc27895942f65b34bd16125b8250aad93216
Ankan Kushwaha <ankankushwaha30@gmail.com>
2025-05-03 15:17:24 +0000

    commit

---
+++ basics/06_Strings.js
@@ -0,0 +1,21 @@
+const name="ankan";
+const repoCount=49;
+
+console.log(name+repoCount+"value"); // concatenation
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName=new String('fifa'); //it will give me key value pair index by index and gives so many properties use this in console of javascript you will get to know
+
+console.log(gameName[1]); // i
+console.log(gameName.__proto__);// it will prototype og gameName which is object
+
+console.log(gameName.length);
+console.log(gameName.toLocaleUpperCase());
+console.log(gameName.charAt(3)); //this method will give us character present at thar particular index
+console.log(gameName.indexOf('f'));// this will 
+
+
+
+
+
